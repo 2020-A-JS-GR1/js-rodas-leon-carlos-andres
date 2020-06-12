dfea1c2a613a68e43552b18400377904b422a8f7
Carlos Rodas <crods57@gmail.com>
2020-06-12 17:17:47 -0500

    Tipos de variables

---
+++ 01-JavaScript/01-variables.js
@@ -0,0 +1,32 @@
+// MUTABLES E INMUTABLES
+    // Mutables
+
+var numeroUno = 1;
+let numeroDos = 2;
+
+numeroUno = 5;
+numeroDos = 8;
+
+numeroUno = false;
+numeroDos = true;
+
+    // Inmutables
+
+const configuracionArchivos = "PDF";
+        // configuracionArchivos = "XML"; <-- no se puede hacer esto porque es const
+
+// TIPOS DE VARIABLES
+
+const numero = 1; // Number
+const sueldo = 1.2; // Number
+const texto = "Hola"; //String
+const booleano = false; // boolean
+const hijos = null; // object
+const zapatos = undefined; // undefined
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof booleano);
+console.log(typeof hijos);
+console.log(typeof zapatos);
