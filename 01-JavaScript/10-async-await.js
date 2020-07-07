916420e9604334d60a1212a5d4e6363c99f6b674
Carlos Rodas <crods57@gmail.com>
2020-07-07 17:04:03 -0500

    Último commit de JS

--- 01-JavaScript/10-async-await.js
+++ 01-JavaScript/10-async-await.js
@@ -1,20 +1,48 @@
-const promesaLeerArchivo = new Promise();
-const promesaEscribirArchivo = new Promise();
+const promesaLeerArchivo = () => {
+    return new Promise(
+        (res, rej) => {
+            res('CONTENIDO LEER ARCHIVO');
+        }
+    );
+}
+
+
+const promesaEscribirArchivo = () => {
+    return new Promise(
+        (res,rej) => {
+            //res('CONTENIDO ESCRIBIR ARCHIVO');
+            rej('ERROR >:(')
+        }
+    );
+}
+
 
 // ASYNC y AWAIT solo dentro de una función
 
 async function ejercicio(){
     console.log('1');
     try {
+        console.log('2');
         const contenidoArchivoActual = await promesaLeerArchivo();
+        console.log(contenidoArchivoActual);
+        console.log('3');
         await promesaEscribirArchivo();
+        console.log('4');
         const nuevoContenido = await promesaLeerArchivo();
         console.log(nuevoContenido);
+        console.log('5');
     } catch (e) {
         console.error(e);
     }
+    console.log('6');
+    console.log('7');
 }
 
-const f = async() => {
-    // contenido
-}
+const respuestaEjercicio = ejercicio();
+
+/*
+console.log('respuestaEjercicio', respuestaEjercicio);
+console.log('promesaLeerArchivo', promesaLeerArchivo());
+console.log('promesaEscribirArchivo', promesaEscribirArchivo());
+ */
+
