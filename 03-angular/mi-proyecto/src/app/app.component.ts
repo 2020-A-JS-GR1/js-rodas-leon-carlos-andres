c8a2d9f2a7c237681c586fa8063d565f14b898ac
Carlos Rodas <crods57@gmail.com>
2020-07-24 17:55:45 -0500

    Fin clase 24/07/2020

--- 03-angular/mi-proyecto/src/app/app.component.ts
+++ 03-angular/mi-proyecto/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'aplicacion-nueva',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
