648a1929487128abd506534b909f83fadb09882c
Carlos Rodas <crods57@gmail.com>
2020-08-25 17:47:32 -0500

    Clase 25/08/2020

--- 04-backend/backend/api/controllers/PokemonController.js
+++ 04-backend/backend/api/controllers/PokemonController.js
@@ -1,12 +1,9 @@
 /**
- * UsuarioController
+ * PokemonController
  *
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-module.exports = {
-
-
-};
+module.exports = {};
 
---
+++ 04-backend/backend/api/models/Batalla.js
@@ -0,0 +1,22 @@
+/**
+ * Batalla.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    attributes: {
+      nombre:{
+        type: 'string',
+        required: true,
+      }
+    }
+
+  },
+
+};
+
