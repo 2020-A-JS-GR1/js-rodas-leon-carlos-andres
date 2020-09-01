738a13d3d9ebb642319706d346594f512ed672a4
Carlos Rodas <crods57@gmail.com>
2020-09-01 18:00:26 -0500

    Clase 01/09/2020 otro commit

--- 03-angular/mi-proyecto/src/app/componentes/carta-pelicula/carta-pelicula.component.ts
+++ 03-angular/mi-proyecto/src/app/componentes/carta-pelicula/carta-pelicula.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-carta-pelicula',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartaPeliculaComponent implements OnInit {
 
+  @Input()
+  urlImagen: string;
+
+  @Input()
+  descripcion: string;
+
+  @Input()
+  nombreBoton: string;
+
+  textoEjemplo = 'Avengers';
+  urlEjemploImagen = 'https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_3adaf4f8.jpeg?region=0,0,540,810';
+  linkTextoEjemplo = 'https://www.google.com';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ejemploFuncion(){
+    alert('Hola!');
+  }
+
+  ejemploBlur(){
+    console.log('Blur!');
   }
 
 }
