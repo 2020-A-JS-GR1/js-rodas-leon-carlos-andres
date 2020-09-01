fc558bc5083d881e143e8d3189b36bfdc44c9e39
Carlos Rodas <crods57@gmail.com>
2020-09-01 18:00:01 -0500

    Clase 01/09/2020

---
+++ 03-angular/mi-proyecto/src/app/componentes/carta-pelicula/carta-pelicula.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carta-pelicula',
+  templateUrl: './carta-pelicula.component.html',
+  styleUrls: ['./carta-pelicula.component.css']
+})
+export class CartaPeliculaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
